2280c2e865f3edc3711afb598b6b628d94b23efa
hannahborel <hannahborel@icloud.com>
2025-03-25 18:06:49 -0500

    Installed Prettier and ESLint integration packages.Updated ESLint config to work with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,29 @@ module.exports = {
     commonjs: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-native/all",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react", "react-native"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
   },
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react-native/no-inline-styles": "warn",
-    "react-native/no-color-literals": "off",
-    "react-native/sort-styles": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-color-literals': 'off',
+    'react-native/sort-styles': 'off',
+    'prettier/prettier': 'error',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
