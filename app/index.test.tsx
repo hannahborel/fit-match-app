f2648b4c643a0942ed43c849cfbd9a75bc53b61f
hannahborel <hannahborel@icloud.com>
2025-03-26 10:02:45 -0500

    updated jest config and packages to fix test run

--- app/index.test.tsx
+++ app/index.test.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { render, screen } from "@testing-library/react-native";
-import Home from "./index";
+import React from 'react';
+import { render, screen } from '@testing-library/react-native';
+import Home from './index';
 
-describe("Home", () => {
-  it("renders the welcome message", () => {
+describe('Home', () => {
+  it('renders the welcome message', () => {
     render(<Home />);
-    const welcomeMessage = screen.getByText(
-      "Open up App.tsx to start working on your app!",
-    );
+    const welcomeMessage = screen.getByText('Open up App.tsx to start working on your app!');
     expect(welcomeMessage).toBeTruthy();
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  preset: "jest-expo",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  preset: 'jest-expo',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+    'node_modules/(?!(jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
-  testPathIgnorePatterns: ["/node_modules/", "/android/", "/ios/"],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '^@/(.*)$': '<rootDir>/app/$1',
   },
+  moduleDirectories: ['node_modules', 'app'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  rootDir: '.',
 };
