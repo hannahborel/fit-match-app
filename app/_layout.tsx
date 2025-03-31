2836250f50c5c1e11ea16603ff5dc5c03a35f4be
hannahborel <hannahborel@icloud.com>
2025-03-31 18:44:55 -0500

    added dark mode and light mode configurations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,47 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {
   MD3LightTheme as DefaultTheme,
   MD3DarkTheme,
   MD3LightTheme,
   PaperProvider,
+  adaptNavigationTheme,
+  IconButton,
 } from 'react-native-paper';
-import { useColorScheme } from 'react-native';
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+
+import { useColorScheme, View } from 'react-native';
 import { themeColors } from '../src/constants/Colors';
+import merge from 'deepmerge';
+import { useState, useCallback } from 'react';
+
+const customDarkTheme = { ...MD3DarkTheme, colors: themeColors.dark };
+const customLightTheme = { ...MD3LightTheme, colors: themeColors.light };
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = merge(LightTheme, customLightTheme);
+const CombinedDarkTheme = merge(DarkTheme, customDarkTheme);
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const paperTheme =
-    colorScheme === 'dark'
-      ? { ...MD3DarkTheme, colors: themeColors.dark }
-      : { ...MD3LightTheme, colors: themeColors.light };
+  const systemColorScheme = useColorScheme();
+  const [isDarkMode, setIsDarkMode] = useState(systemColorScheme === 'dark');
+
+  const toggleTheme = useCallback(() => {
+    setIsDarkMode(!isDarkMode);
+  }, [isDarkMode]);
+
+  const paperTheme = isDarkMode
+    ? { ...MD3DarkTheme, colors: themeColors.dark }
+    : { ...MD3LightTheme, colors: themeColors.light };
+
   return (
     <PaperProvider theme={paperTheme}>
       <SafeAreaProvider>
@@ -28,6 +55,15 @@ export default function RootLayout() {
           <Stack.Screen name="pages/LeagueOptions" />
           <Stack.Screen name="pages/CreateLeague" />
         </Stack>
+        <View style={{ position: 'absolute', top: 50, right: 20 }}>
+          <IconButton
+            icon={isDarkMode ? 'white-balance-sunny' : 'moon-waning-crescent'}
+            size={24}
+            onPress={toggleTheme}
+            mode="contained"
+          />
+        </View>
+        <StatusBar />
       </SafeAreaProvider>
     </PaperProvider>
   );
