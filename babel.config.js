77e20ca4c37e37273061f449e595ddb3ca4184cc
hannahborel <hannahborel@icloud.com>
2025-03-31 16:51:41 -0500

    updated config and removed old dependencies and files no longer being used by glustack after react native paper migration

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,25 @@
-module.exports = function(api) {
-    api.cache(true);
-
-    return {
-        presets: [["babel-preset-expo", {
-            jsxImportSource: "nativewind"
-        }], "nativewind/babel"],
-
-        plugins: [["module-resolver", {
-            root: ["./"],
-
-            alias: {
-                "@": "./",
-                "tailwind.config": "./tailwind.config.js"
-            }
-        }]]
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@': './app',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      ],
+      'react-native-paper/babel',
+      'nativewind/babel',
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+  };
 };
