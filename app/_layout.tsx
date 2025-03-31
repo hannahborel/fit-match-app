68d6e231f03950155234071c35c753fd8147a082
hannahborel <hannahborel@icloud.com>
2025-03-31 18:48:23 -0500

    reverted dark mode light mode button for now

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,49 +1,14 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import {
-  MD3LightTheme as DefaultTheme,
-  MD3DarkTheme,
-  MD3LightTheme,
-  PaperProvider,
-  adaptNavigationTheme,
-  IconButton,
-} from 'react-native-paper';
-import {
-  DarkTheme as NavigationDarkTheme,
-  DefaultTheme as NavigationDefaultTheme,
-} from '@react-navigation/native';
-
-import { useColorScheme, View } from 'react-native';
+import { MD3LightTheme, PaperProvider } from 'react-native-paper';
 import { themeColors } from '../src/constants/Colors';
-import merge from 'deepmerge';
-import { useState, useCallback } from 'react';
 
-const customDarkTheme = { ...MD3DarkTheme, colors: themeColors.dark };
-const customLightTheme = { ...MD3LightTheme, colors: themeColors.light };
-
-const { LightTheme, DarkTheme } = adaptNavigationTheme({
-  reactNavigationLight: NavigationDefaultTheme,
-  reactNavigationDark: NavigationDarkTheme,
-});
-
-const CombinedDefaultTheme = merge(LightTheme, customLightTheme);
-const CombinedDarkTheme = merge(DarkTheme, customDarkTheme);
+const theme = { ...MD3LightTheme, colors: themeColors.light };
 
 export default function RootLayout() {
-  const systemColorScheme = useColorScheme();
-  const [isDarkMode, setIsDarkMode] = useState(systemColorScheme === 'dark');
-
-  const toggleTheme = useCallback(() => {
-    setIsDarkMode(!isDarkMode);
-  }, [isDarkMode]);
-
-  const paperTheme = isDarkMode
-    ? { ...MD3DarkTheme, colors: themeColors.dark }
-    : { ...MD3LightTheme, colors: themeColors.light };
-
   return (
-    <PaperProvider theme={paperTheme}>
+    <PaperProvider theme={theme}>
       <SafeAreaProvider>
         <Stack
           screenOptions={{
@@ -55,14 +20,6 @@ export default function RootLayout() {
           <Stack.Screen name="pages/LeagueOptions" />
           <Stack.Screen name="pages/CreateLeague" />
         </Stack>
-        <View style={{ position: 'absolute', top: 50, right: 20 }}>
-          <IconButton
-            icon={isDarkMode ? 'white-balance-sunny' : 'moon-waning-crescent'}
-            size={24}
-            onPress={toggleTheme}
-            mode="contained"
-          />
-        </View>
         <StatusBar />
       </SafeAreaProvider>
     </PaperProvider>
