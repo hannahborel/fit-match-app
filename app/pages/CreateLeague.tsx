b622a21c66a0b3d62f5ef899c593a975ff412378
hannahborel <hannahborel@icloud.com>
2025-03-31 21:30:57 -0500

    updated label color temporarily for avatar in create league. Will change up later

--- app/pages/CreateLeague.tsx
+++ app/pages/CreateLeague.tsx
@@ -8,24 +8,24 @@ const CreateLeague = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
       <View style={{ marginLeft: 50, marginRight: 50, gap: 24 }}>
-        <TextInput label="League Name" />
+        <TextInput placeholder="League Name" />
         <View style={{ marginLeft: 4 }}>
           <Text>League Size</Text>
           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
             <View style={{ flexDirection: 'row', gap: 20, marginTop: 10 }}>
-              <Avatar.Text size={40} label="4" />
-              <Avatar.Text size={40} label="8" />
-              <Avatar.Text size={40} label="12" />
-              <Avatar.Text size={40} label="16" />
-              <Avatar.Text size={40} label="20" />
-              <Avatar.Text size={40} label="24" />
-              <Avatar.Text size={40} label="28" />
-              <Avatar.Text size={40} label="32" />
-              <Avatar.Text size={40} label="36" />
-              <Avatar.Text size={40} label="40" />
-              <Avatar.Text size={40} label="42" />
-              <Avatar.Text size={38} label="44" />
-              <Avatar.Text size={38} label="48" />
+              <Avatar.Text size={40} label="4" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="8" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="12" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="16" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="20" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="24" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="28" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="32" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="36" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="40" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="42" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={38} label="44" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={38} label="48" labelStyle={{ color: 'black' }} />
             </View>
           </ScrollView>
         </View>
@@ -33,19 +33,19 @@ const CreateLeague = () => {
           <Text>Number of Weeks</Text>
           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
             <View style={{ flexDirection: 'row', gap: 20, marginTop: 10 }}>
-              <Avatar.Text size={40} label="4" />
-              <Avatar.Text size={40} label="8" />
-              <Avatar.Text size={40} label="12" />
-              <Avatar.Text size={40} label="16" />
-              <Avatar.Text size={40} label="20" />
-              <Avatar.Text size={40} label="24" />
-              <Avatar.Text size={40} label="28" />
-              <Avatar.Text size={40} label="32" />
-              <Avatar.Text size={40} label="36" />
-              <Avatar.Text size={40} label="40" />
-              <Avatar.Text size={40} label="42" />
-              <Avatar.Text size={40} label="44" />
-              <Avatar.Text size={40} label="48" />
+              <Avatar.Text size={40} label="4" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="8" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="12" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="16" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="20" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="24" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="28" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="32" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="36" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="40" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="42" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="44" labelStyle={{ color: 'black' }} />
+              <Avatar.Text size={40} label="48" labelStyle={{ color: 'black' }} />
             </View>
           </ScrollView>
         </View>
