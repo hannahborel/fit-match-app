44aa5e09b36655b1a8d8e2f99129825d6cd8451a
hannahborel <hannahborel@icloud.com>
2025-06-24 14:46:35 -0500

    added layout. screen overlays on top

--- src/app/(protected)/(drawer)/_layout.tsx
+++ src/app/(protected)/(drawer)/_layout.tsx
@@ -1,17 +1,20 @@
 
+import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
 import { Slot } from "expo-router";
 import { Drawer } from "expo-router/drawer";
 
-
+function CustomDrawerComntentProps(props: DrawerContentComponentProps) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+   </DrawerContentScrollView>
+  )
+}
 export const DrawerLayout = () => {
   return (
-
-
-          <Drawer><Slot/></Drawer>
-    
-
-     
-    
+    <Drawer screenOptions={{drawerType:'front'}}>
+      <Drawer.Screen name='accountPage' />
+          </Drawer>
   )
 };
 export default DrawerLayout;
