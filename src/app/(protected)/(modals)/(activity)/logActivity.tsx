9ab0e38343ddea3c00346fb3eeafce0bcc3b917f
hannahborel <hannahborel@icloud.com>
2025-06-30 17:05:42 -0500

    removed unused import

--- src/app/(protected)/(modals)/(activity)/logActivity.tsx
+++ src/app/(protected)/(modals)/(activity)/logActivity.tsx
@@ -2,12 +2,7 @@ import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button, IconButton, Text, useTheme } from 'react-native-paper';
 import { NumberScroller } from '@/app/(protected)/(tabs)/activity/components/NumberScroll';
-import {
-  AlignCenter,
-  Image,
-  NotebookText,
-  StickyNote,
-} from 'lucide-react-native';
+import { Image, StickyNote } from 'lucide-react-native';
 
 export default function LogWorkoutScreen() {
   const [minutes, setMinutes] = useState(34);
