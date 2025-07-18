92c2be50e2714584c55b6c0b83066a9745adbe6e
hannahborel <hannahborel@icloud.com>
2025-07-18 13:44:59 -0500

    removed console.logs

--- src/app/(protected)/(tabs)/standings/schedulePage.tsx
+++ src/app/(protected)/(tabs)/standings/schedulePage.tsx
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { leagueAtom } from '@/atoms/leagueAtom';
 import BgView from '@/components/elements/BgView';
-import { differenceInWeeks } from 'date-fns';
+import {
+  getCurrentWeekMatchIds,
+  mapAllWeeksToMatchIds,
+} from '@/helpers/getCurrentWeek';
+import { useAtomValue } from 'jotai';
+import React from 'react';
+import { Text } from 'react-native';
 
 const SchedulePage = () => {
-  const weeks = differenceInWeeks(new Date(2025, 6, 17), new Date(2025, 6, 10));
-  console.log('Weeks', weeks);
+  const leagueData = useAtomValue(leagueAtom);
+  leagueData;
+  if (leagueData) {
+    console.log(mapAllWeeksToMatchIds(leagueData?.matches));
+    console.log(getCurrentWeekMatchIds(leagueData));
+  }
+
   return (
     <BgView>
       <Text>schedule</Text>
