b672cf5070bcac9fece489bf3ac266d08ece2643
hannahborel <hannahborel@icloud.com>
2025-11-16 01:56:53 -0600

    fix: disable unstable package exports to resolve Metro bundling errors

--- metro.config.js
+++ metro.config.js
@@ -11,6 +11,8 @@ config.resolver = {
   ...config.resolver,
   assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
   sourceExts: [...config.resolver.sourceExts, 'svg'],
+  // Disable unstable_enablePackageExports to avoid chunking issues in SDK 53
+  unstable_enablePackageExports: false,
 };
 
 module.exports = config;
