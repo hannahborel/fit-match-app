54d0d47dc4ec9cf483a5de470c565a11e47b535c
hannahborel <hannahborel@icloud.com>
2025-11-20 17:24:42 -0600

    fixed date

--- src/app/(protected)/(tabs)/home/components/InviteFriends/InviteFriendsSection.tsx
+++ src/app/(protected)/(tabs)/home/components/InviteFriends/InviteFriendsSection.tsx
@@ -28,9 +28,9 @@ const InviteFriendsSection: React.FC<InviteFriendsSectionProps> = ({
   // Format the start date
   const formatStartDate = (dateString: string) => {
     const date = new Date(dateString);
-    return date.toLocaleDateString('en-GB', {
-      day: '2-digit',
+    return date.toLocaleDateString('en-US', {
       month: '2-digit',
+      day: '2-digit',
     });
   };
 
